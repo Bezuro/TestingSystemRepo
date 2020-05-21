e962550a5b2d8c9be7dfe222020bd14d44ff2803
Bezuro <serfaksion25@gmail.com>
2020-05-21 18:42:09 +0300

    Added TrueXmlSerializerDLL

---
+++ TrueXmlSerializerDLL/TrueXmlSerializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TrueXmlSerializerDLL
+{
+    public static class TrueXmlSerializer
+    {
+        public static T XmlDeserialize<T>(this string toDeserialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (StringReader textReader = new StringReader(toDeserialize))
+            {
+                return (T)xmlSerializer.Deserialize(textReader);
+            }
+        }
+
+        public static string XmlSerialize<T>(this T toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            }
+        }
+
+        public static void Save<T>(T file, string path)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                xmlSerializer.Serialize(stream, file);
+            }
+        }
+
+        public static T Load<T>(string path)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                return (T)xmlSerializer.Deserialize(stream);
+            }
+        }
+    }
+}
