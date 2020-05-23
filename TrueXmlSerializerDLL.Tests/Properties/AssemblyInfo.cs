1a872f03b1178104e7dd2cd1457a4e0b6b504b37
Bezuro <serfaksion25@gmail.com>
2020-05-23 16:16:53 +0300

    Added UnitTests for TrueXmlSerializer

---
+++ TrueXmlSerializerDLL.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrueXmlSerializerDLL.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrueXmlSerializerDLL.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38c5120b-e24b-4158-9e2f-bda1f9df1067")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TrueXmlSerializerDLL.Tests/TrueXmlSerializerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrueXmlSerializerDLL.Tests
+{
+    [TestClass]
+    public class TrueXmlSerializerTest
+    {
+        [TestMethod]
+        public void SaveAndLoadTest()
+        {
+            List<string> list = new List<string>();
+            list.Add("qwerty");
+            list.Add("asdfgh");
+            list.Add("zxcvbn");
+
+            string path = "trueXmlSerializerTest.xml";
+
+            TrueXmlSerializer.Save(list, path);
+
+            List<string> list2 = TrueXmlSerializer.Load<List<string>>(path);
+
+            CollectionAssert.AreEqual(list, list2);
+        }
+
+        [TestMethod]
+        public void SerializationAndDeserializationTest()
+        {
+            List<string> list = new List<string>();
+            list.Add("qwerty");
+            list.Add("asdfgh");
+            list.Add("zxcvbn");
+
+            string serializedList = TrueXmlSerializer.XmlSerialize(list);
+
+            List<string> list2 = TrueXmlSerializer.XmlDeserialize<List<string>>(serializedList);
+
+            CollectionAssert.AreEqual(list, list2);
+        }
+    }
+}
